f4b1c63f96a97a210da5071fc7ded553353da563
Nikulin Ivan <103510364+NikulinGIT@users.noreply.github.com>
2022-07-16 00:16:03 +0300

    lesson2

--- ITMO_producing_apps_Csharp/Lab1/Form1.cs
+++ ITMO_producing_apps_Csharp/Lab1/Form1.cs
@@ -9,7 +9,10 @@ namespace Lab1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(0, 0),new Point(0, this.Height),new Point(this.Width, 0) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,6 +30,12 @@ namespace Lab1
         private void button3_Click(object sender, EventArgs e)
         {
             this.Opacity = 1;
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
 
         }
     }
